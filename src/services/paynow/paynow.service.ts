541ea5c7028eb23c502e87d8e8a733b48694a982
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-07-10 22:00:06 +0200

    Add paynow status route

--- src/services/paynow/paynow.service.ts
+++ src/services/paynow/paynow.service.ts
@@ -1,7 +1,7 @@
 // Initializes the `paynow` service on path `/paynow`
 import { ServiceAddons } from '@feathersjs/feathers';
 import { Application } from '../../declarations';
-import { Paynow } from './paynow.class';
+import { Paynow, PaynowStatus } from './paynow.class';
 import createModel from '../../models/paynow.model';
 import hooks from './paynow.hooks';
 
@@ -20,6 +20,7 @@ export default function (app: Application): void {
 
   // Initialize our service with any options it requires
   app.use('/paynow', new Paynow(options, app));
+  app.use('/paynow/status-update', new PaynowStatus(options, app));
 
   // Get our initialized service so that we can register hooks
   const service = app.service('paynow');
