7ed12a3fcebd2731a9a6bff83532e131e3569ba1
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-07-10 12:54:30 +0200

    Init Project

---
+++ README.md
@@ -0,0 +1,28 @@
+# Papapi
+
+Papai is a simple, fast, and powerful microservice for Paynow, PayPal and Stripe(To come) payments.
+
+# Installation
+
+1. Clone it
+
+```bash
+$ git clone https://github.com/takumade/papapi
+```
+
+2. Install dependencies
+
+```bash
+$ cd papapi
+$ npm install
+```
+**Note:** You can use yarn or the new bun. If something breaks with the bun, you are on your own.
+
+3. Config your environment variables in `.env`
+
+```
+PAYNOW_INTEGRATION_ID=<your integration id>
+PAYNOW_SECRET_KEY=<your secret key>
+```
+
+To be continued...
---
+++ src/models/users.model.ts
@@ -0,0 +1,37 @@
+// See https://sequelize.org/master/manual/model-basics.html
+// for more of what you can do here.
+import { Sequelize, DataTypes, Model } from 'sequelize';
+import { Application } from '../declarations';
+import { HookReturn } from 'sequelize/types/hooks';
+
+export default function (app: Application): typeof Model {
+  const sequelizeClient: Sequelize = app.get('sequelizeClient');
+  const users = sequelizeClient.define('users', {
+  
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  
+  
+  }, {
+    hooks: {
+      beforeCount(options: any): HookReturn {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (users as any).associate = function (models: any): void {
+    // Define associations here
+    // See https://sequelize.org/master/manual/assocs.html
+  };
+
+  return users;
+}
