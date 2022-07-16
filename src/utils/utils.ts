3b0bb4cc30092ef35360fcdcdea1eedee84ab6e7
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-07-16 23:42:32 +0200

    Add send to webhook url method

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,14 +1,15 @@
 
+import axios from 'axios';
 import crypto from 'crypto';
 
 
 export const objectHasKeys = (keys: any[], object: {}) => {
 
     let objectKeys = Object.keys(object)
-    let status = true 
+    let status = true
 
     keys.map(k => {
-        if (!objectKeys.includes(k)){
+        if (!objectKeys.includes(k)) {
             status = false
         }
     })
@@ -17,5 +18,19 @@ export const objectHasKeys = (keys: any[], object: {}) => {
 }
 
 export const generateTransactionId = () => {
-    return "TRX-"+new Date().getTime()+"-"+crypto.randomBytes(8).toString('hex')
+    return "TRX-" + new Date().getTime() + "-" + crypto.randomBytes(8).toString('hex')
+}
+
+export const pushToWebhook = async (origin: string, type: string, url: string, data: any) => {
+    if (url && url.length > 0) {
+
+        let webhookData = {
+            origin,
+            type,
+            data,
+        }
+
+        // NOTE: Should await these calls or not?
+        await axios.post(url, webhookData)
+    }
 }
