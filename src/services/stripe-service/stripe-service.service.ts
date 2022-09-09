d36db067d5ba78f976f5c2758241952cf527802c
Takunda Madechangu <takundam@sonity.net>
2022-09-09 22:11:40 +0200

    Add stripe webshook endpoint

--- src/services/stripe-service/stripe-service.service.ts
+++ src/services/stripe-service/stripe-service.service.ts
@@ -18,12 +18,13 @@ export default function (app: Application): void {
     paginate: app.get('paginate')
   };
 
-  const stripeService =  new StripeService(options, app)
+  const stripeService =  new StripeService(options, app);
 
   // Initialize our service with any options it requires
   app.use('/stripe-service', stripeService);
-  app.post('/stripe/create-customer', stripeService.createCustomer)
-  app.post('/stripe/payment', stripeService.createPayment)
+  app.post('/stripe/create-customer', stripeService.createCustomer);
+  app.post('/stripe/payment', stripeService.createPayment);
+  app.post('/stripe/webhooks', stripeService.webhooks);
 
   // Get our initialized service so that we can register hooks
   const service = app.service('stripe-service');
