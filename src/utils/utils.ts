81e1d186d098e7f9c17cdc3b46e7f01832c4bb03
Takunda Madechangu <takundam@sonity.net>
2022-09-10 20:23:38 +0200

    Add payment statuses

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,32 +5,39 @@ import crypto from 'crypto';
 
 export const objectHasKeys = (keys: any[], object: {}) => {
 
-    let objectKeys = Object.keys(object)
-    let status = true
+  const objectKeys = Object.keys(object);
+  let status = true;
 
-    keys.map(k => {
-        if (!objectKeys.includes(k)) {
-            status = false
-        }
-    })
+  keys.map(k => {
+    if (!objectKeys.includes(k)) {
+      status = false;
+    }
+  });
 
-    return status
-}
+  return status;
+};
 
 export const generateTransactionId = () => {
-    return "TRX-" + new Date().getTime() + "-" + crypto.randomBytes(8).toString('hex')
-}
+  return 'TRX-' + new Date().getTime() + '-' + crypto.randomBytes(8).toString('hex');
+};
 
 export const pushToWebhook = async (origin: string, type: string, url: string, data: any) => {
-    if (url && url.length > 0) {
+  if (url && url.length > 0) {
 
-        let webhookData = {
-            origin,
-            type,
-            data,
-        }
+    const webhookData = {
+      origin,
+      type,
+      data,
+    };
 
-        // NOTE: Should await these calls or not?
-        await axios.post(url, webhookData)
-    }
-}
+    // NOTE: Should await these calls or not?
+    await axios.post(url, webhookData);
+  }
+};
+
+export const paymentStatuses = {
+  session_created: 'SESSION_CREATED',
+  paid: 'PAID',
+  failed: 'FAILED',
+  cancelled: 'CANCELLED',
+};
