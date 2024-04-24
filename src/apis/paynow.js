7dcc1cbc32581a7770543ada7f81cddebbc96fcc
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-25 00:43:03 +0200

    Feed paynow entity to apis

--- src/apis/paynow.js
+++ src/apis/paynow.js
@@ -1,10 +1,17 @@
-export const createPayment = (req, res, context) => {
-    res.set("Access-Control-Allow-Origin", "*"); // Example of modifying headers to override Wasp default CORS middleware.
+export const createPayment = async (req, res, context) => {
+    res.set("Access-Control-Allow-Origin", "*"); 
+    
+    let paynowEntity = await context.entities.Paynow 
+    
+    // Example of modifying headers to override Wasp default CORS middleware.
     res.json({ msg: `Hello, ${context.user ? "registered user" : "stranger"}!` });
   };
 
 
-  export const statusUpdate = (req, res, context) => {
+  export const statusUpdate = async (req, res, context) => {
     res.set("Access-Control-Allow-Origin", "*"); // Example of modifying headers to override Wasp default CORS middleware.
+    
+    let paynowEntity = await context.entities.Paynow 
+    
     res.json({ msg: `Hello, ${context.user ? "registered user" : "stranger"}!` });
   };
