2764195bf4b474169d13f1c330ab05e25a0d9acf
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-01 10:40:33 +0200

    Return response from paynow to user

--- src/apis/paynow.ts
+++ src/apis/paynow.ts
@@ -9,14 +9,16 @@ export const createPayment = async (req: Request, res: Response, context: any) =
     let response = paynow.create(req.body)
 
     // Example of modifying headers to override Wasp default CORS middleware.
-    res.json({ msg: `Hello, ${context.user ? "registered user" : "stranger"}!` });
+    res.json(response);
   };
 
 
   export const statusUpdate = async (req: Request, res: Response, context: any) => {
     res.set("Access-Control-Allow-Origin", "*"); // Example of modifying headers to override Wasp default CORS middleware.
+
+    let paynow = new Paynow()
+    let response = paynow.statusUpdate(req, res)
     
-    let paynowEntity = await context.entities.Paynow 
-    
-    res.json({ msg: `Hello, ${context.user ? "registered user" : "stranger"}!` });
+    // res.json({ msg: `Hello, ${context.user ? "registered user" : "stranger"}!` });
+    res.json(response);
   };
