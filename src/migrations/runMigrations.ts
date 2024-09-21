78a49185ce78352f9913f62c9c6ec28da7db88bb
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-22 01:43:47 +0200

    Execute db migrations

---
+++ src/migrations/runMigrations.ts
@@ -0,0 +1,51 @@
+import * as path from 'path'
+import { Pool } from 'pg'
+import { promises as fs } from 'fs'
+import {
+  Kysely,
+  Migrator,
+  PostgresDialect,
+  FileMigrationProvider,
+} from 'kysely'
+import { Database } from '../types'
+
+async function migrateToLatest() {
+  const db = new Kysely<Database>({
+    dialect: new PostgresDialect({
+      pool: new Pool({
+        host: 'localhost',
+        database: 'kysely_test',
+      }),
+    }),
+  })
+
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      // This needs to be an absolute path.
+      migrationFolder: path.join(__dirname, './migrations'),
+    }),
+  })
+
+  const { error, results } = await migrator.migrateToLatest()
+
+  results?.forEach((it) => {
+    if (it.status === 'Success') {
+      console.log(`migration "${it.migrationName}" was executed successfully`)
+    } else if (it.status === 'Error') {
+      console.error(`failed to execute migration "${it.migrationName}"`)
+    }
+  })
+
+  if (error) {
+    console.error('failed to migrate')
+    console.error(error)
+    process.exit(1)
+  }
+
+  await db.destroy()
+}
+
+migrateToLatest()
