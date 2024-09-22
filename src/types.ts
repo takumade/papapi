5a6f75d0e7ab35cbf2220ec1fc792b0a87d245bb
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-22 02:05:34 +0200

    Add more types

--- src/types.ts
+++ src/types.ts
@@ -8,37 +8,98 @@ import {
   } from 'kysely'
   
   export interface Database {
-    person: PersonTable
-    pet: PetTable
+    user: UserTable
+    task: TaskTable
+    stripe: StripeTable
+    paypal: PaypalTable
+    paynow: PaynowTable
+    paynow_status_update: PaynowStatusUpdateTable
   }
   
-
-  export interface PersonTable {
-
+  export interface UserTable {
     id: Generated<number>
-    first_name: string
-    gender: 'man' | 'woman' | 'other'
-    last_name: string | null
-    created_at: ColumnType<Date, string | undefined, never>
-    metadata: JSONColumnType<{
-      login_at: string
-      ip: string | null
-      agent: string | null
-      plan: 'free' | 'premium'
-    }>
   }
   
-  export type Person = Selectable<PersonTable>
-  export type NewPerson = Insertable<PersonTable>
-  export type PersonUpdate = Updateable<PersonTable>
+  export type User = Selectable<UserTable>
+  export type NewUser = Insertable<UserTable>
+  export type UserUpdate = Updateable<UserTable>
   
-  export interface PetTable {
+  export interface TaskTable {
     id: Generated<number>
-    name: string
-    owner_id: number
-    species: 'dog' | 'cat'
+    description: string
+    is_done: ColumnType<boolean, boolean | undefined, never> // since `is_done` has a default
+    user_id: number | null
   }
   
-  export type Pet = Selectable<PetTable>
-  export type NewPet = Insertable<PetTable>
-  export type PetUpdate = Updateable<PetTable>
+  export type Task = Selectable<TaskTable>
+  export type NewTask = Insertable<TaskTable>
+  export type TaskUpdate = Updateable<TaskTable>
+  
+  export interface StripeTable {
+    id: Generated<number>
+    session_id: string
+    transaction_id: string
+    invoice: string
+    email: string
+    phone: string
+    method: string
+    items: string
+    session: string
+    amount: number
+    success_url: string
+    cancel_url: string
+    status: string
+  }
+  
+  export type Stripe = Selectable<StripeTable>
+  export type NewStripe = Insertable<StripeTable>
+  export type StripeUpdate = Updateable<StripeTable>
+  
+  export interface PaypalTable {
+    id: Generated<number>
+    order_id: string
+    transaction_id: string
+    invoice: string
+    currency: string
+    email: string
+    method: string
+    items: string
+    amount: number
+    status: string
+  }
+  
+  export type Paypal = Selectable<PaypalTable>
+  export type NewPaypal = Insertable<PaypalTable>
+  export type PaypalUpdate = Updateable<PaypalTable>
+  
+  export interface PaynowTable {
+    id: Generated<number>
+    transaction_id: string
+    invoice: string
+    paynow_reference: string
+    instructions: string
+    email: string
+    phone: string
+    method: string
+    items: string
+    amount: number
+    redirect_url: string
+    result_url: string
+    link_url: string
+    poll_url: string
+    status: string
+  }
+  
+  export type Paynow = Selectable<PaynowTable>
+  export type NewPaynow = Insertable<PaynowTable>
+  export type PaynowUpdate = Updateable<PaynowTable>
+  
+  export interface PaynowStatusUpdateTable {
+    id: Generated<number>
+    text: string
+  }
+  
+  export type PaynowStatusUpdate = Selectable<PaynowStatusUpdateTable>
+  export type NewPaynowStatusUpdate = Insertable<PaynowStatusUpdateTable>
+  export type PaynowStatusUpdateUpdate = Updateable<PaynowStatusUpdateTable>
+  
