148de016186e211cb8282e45d4c2c7c9bc71c1ec
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-23 23:56:09 +0200

    Place code inside try catch

--- src/apis/paynow.ts
+++ src/apis/paynow.ts
@@ -6,12 +6,15 @@ const paynow = new Hono()
 
 
 paynow.post('/create-payment', async (c) => {
+  try{
+      let paynow = new PaynowLib()
+      const body = await c.req.json()
+      let response = await paynow.create(body)
 
-  let paynow = new PaynowLib()
-  const body = await c.req.json()
-  let response = await paynow.create(body)
-
-  return c.json(response)
+      return c.json(response)
+  }catch(error: any){
+    return c.json({status: false,message: "Cannot process request", error: error.message}, 500)
+  }
 })
 
 paynow.post('/status-update', async (c) => {
