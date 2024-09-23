96d7bd243c1578e033e8c86a81ce6f2febec4b73
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-23 23:35:39 +0200

    Create contants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,18 @@
+export const paymentStatuses = {
+    session_created: 'SESSION_CREATED',
+    paid: 'PAID',
+    failed: 'FAILED',
+    cancelled: 'CANCELLED',
+    delivered: 'DELIVERED',
+    awaitingDelivery: 'AWAITING_DELIVERY',
+    awaitingPayment: 'AWAITING_PAYMENT',
+    refunded: 'REFUNDED',
+    disputed: 'DISPUTED'
+  };
+  
+  export const paymentMethods = {
+    stripe: 'STRIPE',
+    paypal: 'PAYPAL',
+    paynow: 'PAYNOW'
+  };
+  
