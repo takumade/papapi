815a5656987fa6681b760509a6fc7dcecfdf4443
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-24 01:57:33 +0200

    Parse body instead of converting to json

--- src/apis/paynow.ts
+++ src/apis/paynow.ts
@@ -1,6 +1,7 @@
 
 import { Hono } from "hono"
 import { PaynowLib } from "../lib/paynow";
+import { PaynowStatus } from "../utils/constants";
 
 const paynow = new Hono()
 
@@ -20,12 +21,14 @@ paynow.post('/create-payment', async (c) => {
 paynow.post('/status-update', async (c) => {
 
   try{
-    const body = await c.req.json()
+
+    const body: PaynowStatus = await c.req.parseBody()   
     let paynow = new PaynowLib()
     let response = await paynow.statusUpdate(body)
 
     return c.json(response)
   }catch(error: any){
+    console.log("Error: ", error)
     return c.json({status: false,message: "Cannot process request", error: error.message}, 500)
   }
 })
