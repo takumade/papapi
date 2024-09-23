42513f0f572977871131b19c4033ddddc994cb92
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-24 01:57:47 +0200

    update payment status

--- src/lib/paynow.ts
+++ src/lib/paynow.ts
@@ -1,5 +1,5 @@
-import { createTransaction } from '../repositories/transaction';
-import { PaymentMethods, PaymentStatuses } from '../utils/constants';
+import { createTransaction, findTransaction, updateTransaction } from '../repositories/transaction';
+import { PaymentMethods, PaymentStatuses, PaynowStatus } from '../utils/constants';
 import { generateTransactionId, json, objectHasKeys, pushToWebhook, sleep } from '../utils/utils';
 import { Paynow } from "paynow"
 import { Context } from 'hono';
@@ -14,18 +14,18 @@ export class PaynowLib {
   constructor() {
 
     // Init Paynow 
-    
+
     this.paynow = new Paynow(
-      process.env.PAYNOW_INTEGRATION_ID as string, 
-      process.env.PAYNOW_INTEGRATION_KEY as string, 
+      process.env.PAYNOW_INTEGRATION_ID as string,
+      process.env.PAYNOW_INTEGRATION_KEY as string,
       process.env.PAYNOW_INTEGRATION_RESULT_URL as string,
       process.env.PAYNOW_INTEGRATION_RETURN_URL as string
     );
-    
+
     this.paynow.email = process.env.PAYNOW_INTEGRATION_EMAIL;
   }
 
- 
+
 
   async create(data: any) {
 
@@ -33,7 +33,7 @@ export class PaynowLib {
 
     console.log('[Create Paynow Payment] Data: ', data);
     console.log('[Create Paynow Payment] Paynow: ', this.paynow);
-    
+
 
     const checkKeys = [
       'email',
@@ -90,15 +90,15 @@ export class PaynowLib {
         const linkUrl = response?.linkUrl;
         let transaction = await this.paynow.pollTransaction(pollUrl);
 
-        
+
         let retries = 0
 
-        while (retries <= 3){
+        while (retries <= 3) {
           await sleep(4000)
 
           transaction = await this.paynow.pollTransaction(pollUrl);
 
-          if (transaction.status == "Paid" || transaction.status == "Cancelled") break          
+          if (transaction.status == "Paid" || transaction.status == "Cancelled") break
           retries++
         }
 
@@ -151,70 +151,61 @@ export class PaynowLib {
     }
   }
 
-  statusUpdate = async (status_data: any) => {
-
+  statusUpdate = async (status_data: PaynowStatus) => {
     const statusData = status_data
-
     if (objectHasKeys(['reference', 'paynowreference', 'pollurl'], statusData)) {
 
       try {
         console.log('Paynow Data: ', statusData);
+        const transaction = await findTransaction(
+          {
+            invoice: statusData.reference,
+            paynow_reference: statusData.paynowreference
+          }, PaymentMethods.Paynow
+        );
 
+        if (transaction) {
+          const id = transaction.id;
 
-        // const transaction = await paynow.findOne({
-        //   where: {
-        //     paynowReference: statusData.paynowreference,
-        //     invoice: statusData.reference
-        //   }
-        // });
+          transaction.status = this.retrievePaynowStatus(statusData.status);
+          await updateTransaction(id, PaymentMethods.Paynow, { status: transaction.status });
 
-    
+          // Send an update
+          const webhookUrl = '' // TODO: Add your webhook url here
 
-        // if (transaction) {
-        //   const id = transaction.id;
-
-        //   const status:string  = this.retrievePaynowStatus(statusData.status);
-
-        //   const response = await paynow.update({
-        //     status: status
-        //   }, {
-        //     where: {
-        //       id: id
-        //     }
-        //   });
-
-        //   const updatedData = await paynow.findByPk(id);
-
-        //   // Send an update
-        //   const webhookUrl = this.app.get('paynow').webhookUrl;
-
-        //   pushToWebhook(
-        //     'papapi',
-        //     'paynow-status-update',
-        //     webhookUrl, updatedData
-        //   );
-
-        //   if (res == null){
-        //     return updatedData;
-        //   }
-
-          
+          if (webhookUrl.length > 0) {
+            pushToWebhook(
+              'papapi',
+              'paynow-status-update',
+              webhookUrl, transaction
+            );
+          }
 
           return {
             'status': true,
             'message': 'Status updated successfully',
             // data: updatedData
           }
-        }catch (error: any) {
+        } else {
+          return {
+            'status': false,
+            'message': 'Transaction not found'
+          }
 
+        }
+      } catch (error: any) {
         return {
           'status': false,
-          'message': 'The status wasnt updated successfully',
-          error: error.message
-        }
+          'message': 'There is some error somewhere',
+          error: error
+        };
       }
     }
-  };
+    return {
+      'status': false,
+      'message': 'Please supply these items: reference, paynowreference, pollurl'
+    };
+  }
 
   private retrievePaynowStatus(status: string) {
     if (status === 'Paid') {
@@ -233,7 +224,9 @@ export class PaynowLib {
       status = PaymentStatuses.Refunded;
     } else if (status === 'Disputed') {
       status = PaymentStatuses.Disputed;
-    } 
+    } else if (status === 'Sent') {
+      status = PaymentStatuses.Sent;
+    }
     return status;
   }
 }
