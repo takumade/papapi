8ad3413f1de0c7447cade546f7d2840721f7d740
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-27 23:23:38 +0200

    Add create customer endpoint

--- src/apis/stripe.ts
+++ src/apis/stripe.ts
@@ -1,21 +1,21 @@
 
 import { Hono } from "hono"
 import { Paypal } from "../lib/paypal";
+import { StripeAPI } from "../lib/stripe";
 
-const paypal = new Hono()
+const stripe = new Hono()
 
 // app.post('/stripe/create-customer', stripeService.createCustomer);
 // app.post('/stripe/payment', stripeService.createPayment);
 // app.use('/stripe/webhooks', express.raw({type: 'application/json'}), stripeService.webhooks);
 
 
-paypal.post('/orders', async (c) => {
-
+stripe.post('/create-customer', async (c) => {
   try{
 
     const body = await c.req.json()   
-    let pp = new Paypal()
-    let response = await pp.createOrder(body)
+    let stripeService = new StripeAPI()
+    let response = await stripeService.createCustomer(body)
 
     return c.json(response)
   }catch(error: any){
@@ -24,7 +24,23 @@ paypal.post('/orders', async (c) => {
   }
 })
 
-paypal.post('/orders/:order_id/capture', async (c) => {
+
+stripe.post('/create-customer', async (c) => {
+
+    try{
+  
+      const body = await c.req.json()   
+      let stripeService = new StripeAPI()
+      let response = await stripeService.createCustomer(body)
+  
+      return c.json(response)
+    }catch(error: any){
+      console.log("Error: ", error)
+      return c.json({status: false,message: "Cannot process request", error: error.message}, 500)
+    }
+  })
+
+stripe.post('/orders/:order_id/capture', async (c) => {
 
     try{
 
@@ -41,4 +57,4 @@ paypal.post('/orders/:order_id/capture', async (c) => {
   })
 
   
-export default paypal  
+export default stripe  
