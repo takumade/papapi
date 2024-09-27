8ddd660a0634fd1ef5f633f4b153b6dfc1529fde
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-27 23:28:34 +0200

    Finish webhooks endpoint

--- src/apis/stripe.ts
+++ src/apis/stripe.ts
@@ -11,50 +11,47 @@ const stripe = new Hono()
 
 
 stripe.post('/create-customer', async (c) => {
-  try{
+    try {
 
-    const body = await c.req.json()   
-    let stripeService = new StripeAPI()
-    let response = await stripeService.createCustomer(body)
+        const body = await c.req.json()
+        let stripeService = new StripeAPI()
+        let response = await stripeService.createCustomer(body)
 
-    return c.json(response)
-  }catch(error: any){
-    console.log("Error: ", error)
-    return c.json({status: false,message: "Cannot process request", error: error.message}, 500)
-  }
+        return c.json(response)
+    } catch (error: any) {
+        console.log("Error: ", error)
+        return c.json({ status: false, message: "Cannot process request", error: error.message }, 500)
+    }
 })
 
 
 stripe.post('/payment', async (c) => {
 
-    try{
-  
-      const body = await c.req.json()   
-      let stripeService = new StripeAPI()
-      let response = await stripeService.createPayment(body)
-  
-      return c.json(response)
-    }catch(error: any){
-      console.log("Error: ", error)
-      return c.json({status: false,message: "Cannot process request", error: error.message}, 500)
+    try {
+
+        const body = await c.req.json()
+        let stripeService = new StripeAPI()
+        let response = await stripeService.createPayment(body)
+
+        return c.json(response)
+    } catch (error: any) {
+        console.log("Error: ", error)
+        return c.json({ status: false, message: "Cannot process request", error: error.message }, 500)
     }
-  })
+})
 
-stripe.post('/orders/:order_id/capture', async (c) => {
-
-    try{
-
-      const {order_id} = c.req.param()
-  
-      let pp = new Paypal()
-      let response = await pp.captureOrder(order_id)
-  
-      return c.json(response)
-    }catch(error: any){
-      console.log("Error: ", error)
-      return c.json({status: false,message: "Cannot process request", error: error.message}, 500)
+stripe.post('/webhooks', async (c) => {
+    try {
+        const body = await c.req.parseBody()
+        const stripeSignature: string = c.req.header("stripe-signature") as string
+        let stripeService = new StripeAPI()
+        let response = await stripeService.webhooks(stripeSignature, body)
+        return c.json(response)
+    } catch (error: any) {
+        console.log("Error: ", error)
+        return c.json({ status: false, message: "Cannot process request", error: error.message }, 500)
     }
-  })
+})
 
-  
+
 export default stripe  
