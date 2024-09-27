1f2c6b982a02ebf98858d508c439ace653aa37d9
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-27 22:56:55 +0200

    Add endpoint comments

---
+++ src/apis/stripe.ts
@@ -0,0 +1,44 @@
+
+import { Hono } from "hono"
+import { Paypal } from "../lib/paypal";
+
+const paypal = new Hono()
+
+// app.post('/stripe/create-customer', stripeService.createCustomer);
+// app.post('/stripe/payment', stripeService.createPayment);
+// app.use('/stripe/webhooks', express.raw({type: 'application/json'}), stripeService.webhooks);
+
+
+paypal.post('/orders', async (c) => {
+
+  try{
+
+    const body = await c.req.json()   
+    let pp = new Paypal()
+    let response = await pp.createOrder(body)
+
+    return c.json(response)
+  }catch(error: any){
+    console.log("Error: ", error)
+    return c.json({status: false,message: "Cannot process request", error: error.message}, 500)
+  }
+})
+
+paypal.post('/orders/:order_id/capture', async (c) => {
+
+    try{
+
+      const {order_id} = c.req.param()
+  
+      let pp = new Paypal()
+      let response = await pp.captureOrder(order_id)
+  
+      return c.json(response)
+    }catch(error: any){
+      console.log("Error: ", error)
+      return c.json({status: false,message: "Cannot process request", error: error.message}, 500)
+    }
+  })
+
+  
+export default paypal  
